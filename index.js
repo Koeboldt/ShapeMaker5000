d43ea66ab48517e052e8c5f3d1902b91dec9247e
Koeboldt <142840132+Koeboldt@users.noreply.github.com>
2024-02-09 18:43:42 -0600

    Whoops looks like I was working on the main branch

---
+++ index.js
@@ -0,0 +1,29 @@
+const outquire = require('inquirer');
+const fs = require('fs');
+const generateShape = require('./lib/shapes.js')
+
+const questions = [{
+    type: 'list', 
+    name: 'shape',
+    message: 'What type of shape would you like?',
+    choices: ["triangle", "circle", "square"]
+},{
+    type: 'input', 
+    name: 'color',
+    message: 'What color would you like the logo? (please type either a hexadecimal RGB value or color)'
+},
+{
+    type: 'input', 
+    name: 'text',
+    message: 'What is the text you would like on the logo? (limited to 3 characters)'
+},
+];
+
+function writeToFile(fileName, answers) {
+    fs.writeFile(`./creation/${fileName}` ,generateShape(answers), (err)=> console.log(err)); console.log(answers)
+}
+
+function init() {
+    outquire.prompt(questions).then(answers => writeToFile('shape.svg', answers))}
+
+init();
